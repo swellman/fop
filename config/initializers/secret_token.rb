3dc27065f03afed26a0395cd5275091b57f51a15
Steven Wellman <stevenwellman@gmail.com>
2013-03-19 17:04:42 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fop::Application.config.secret_token = '3077f6b24749756e32d0495172c32d88247d2217510894130f60061b149eb293616c8e720eb997262478cffbee08bf21944e7ba1b5261bbbe31d54d36081d351'
